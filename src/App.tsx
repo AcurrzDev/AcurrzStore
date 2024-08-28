bd2c1b227f85e5711a1863a2a94f6d98e6fc26ee
acurrzdev <everlastingvoyageroblox@gmail.com>
2024-08-28 02:59:54 -0500

    Hash router update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navigator from "./components/Navigator";
 
@@ -12,7 +12,7 @@ import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/AcurrzStore">
       <Navigator />
       <Routes>
         <Route path="/home" element={<Home />} />
