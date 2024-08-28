b3bebf9073294c5a163537faf88ce25a59840244
acurrzdev <everlastingvoyageroblox@gmail.com>
2024-08-28 02:00:20 -0500

    Crazy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello, Customers!</h1>
+    </div>
+  );
 }
 
 export default App;
